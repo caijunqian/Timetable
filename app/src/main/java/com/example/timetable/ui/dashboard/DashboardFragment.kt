123aee4a7b850bbdec6a251e754085f9e4096e71
林俊杰 <2638498951@qq.com>
2021-01-07 23:10:41 +0800

    实现轮询推送消息

--- app/src/main/java/com/example/timetable/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timetable/ui/dashboard/DashboardFragment.kt
@@ -9,22 +9,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.timetable.R
-import com.example.timetable.Subject
-import com.example.timetable.SubjectRepertory
+import com.example.timetable.*
 import com.zhuangfei.timetable.TimetableView
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), View.OnClickListener {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     private var mTimetableView: TimetableView? = null
     private var mySubjects:List<Subject>? = null
+    private var searchSubjects:List<Subject>? = null
     private var alertDialog: AlertDialog? = null
+
+    private var curWeekBtn :Button?=null
+    private var preWeekBtn: Button? = null
+    private var nextWeekBtn:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,13 +39,20 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        //获取控件
+        //获取课程表控件
         mTimetableView = root.findViewById(R.id.id_timetableView)
-        mTimetableView!!.maxSlideItem(11).itemHeight(65)
+        mTimetableView!!.maxSlideItem(11).itemHeight(59)
         mTimetableView!!.showView()
+
+        //获取按钮控件
+        preWeekBtn = root.findViewById(R.id.btn_preWeek)
+        preWeekBtn!!.setOnClickListener(this)
+        nextWeekBtn = root.findViewById(R.id.btn_nextWeek)
+        nextWeekBtn!!.setOnClickListener(this)
+        curWeekBtn = root.findViewById(R.id.btn_curWeek)
+        curWeekBtn!!.setOnClickListener(this)
         requestData()
         return root
     }
@@ -63,6 +74,7 @@ class DashboardFragment : Fragment() {
             handler.sendEmptyMessage(0x123)
         }.start()
     }
+
     private var handler: Handler = @SuppressLint("HandlerLeak")
     object : Handler() {
         override fun handleMessage(msg: Message?) {
@@ -72,6 +84,27 @@ class DashboardFragment : Fragment() {
             Log.d("jj", mySubjects.toString());
             mTimetableView!!.source(mySubjects).showView()
         }
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.btn_preWeek->{
+                Log.d("jj","上一周")
+                searchSubjects = SubjectRepertory.loadDefaultSubjects()
+                mTimetableView!!.source(searchSubjects).showView()
+            }
+            R.id.btn_nextWeek->{
+                Log.d("jj","下一周")
+                searchSubjects = SubjectRepertory.loadDefaultSubjects()
+                mTimetableView!!.source(searchSubjects).showView()
+            }
+            R.id.btn_curWeek->{
+                mTimetableView!!.source(mySubjects).showView()
+
+
+            }
+        }
+
     }
 
 
