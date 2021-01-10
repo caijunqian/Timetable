80182b1c713f56db4d823b6cdfe8f954798be57e
林俊杰 <2638498951@qq.com>
2021-01-10 23:14:35 +0800

    实现了课程表的网络获取以及上下周按钮的操作

--- app/src/main/java/com/example/timetable/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timetable/ui/dashboard/DashboardFragment.kt
@@ -1,20 +1,14 @@
 package com.example.timetable.ui.dashboard
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.timetable.*
 import com.example.timetable.httpReq.*
