06b2c8d73694408ebe04513261e54f46bf98f847
caijunqian <1522617876@qq.com>
2021-01-11 19:51:16 +0800

    实现了删除notice功能

--- app/src/main/java/com/example/timetable/ui/notifications/NotificationsAdapter.kt
+++ app/src/main/java/com/example/timetable/ui/notifications/NotificationsAdapter.kt
@@ -16,18 +16,22 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.RecyclerView
+import com.example.timetable.GlobalMsg
 import com.example.timetable.R
+import com.example.timetable.httpReq.ItemApi
 import com.example.timetable.httpReq.ItemBean
+import com.example.timetable.httpReq.RetrofitUtils
 import kotlinx.android.synthetic.main.cell.view.*
 import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class NotificationsAdapter : RecyclerView.Adapter<NotificationsAdapter.MyViewHolder>(){
-//    var allEntrys :List<Entry>?=null
-//    var dao:EntryDao ?=null
     var notifications:List<ItemBean.Data>?=null
     class MyViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
         var textViewPos:TextView?=itemView.findViewById(R.id.textViewPos)
@@ -56,17 +60,24 @@ class NotificationsAdapter : RecyclerView.Adapter<NotificationsAdapter.MyViewHol
             val endTime = sdf2.format(date)
             holder.textViewEntry!!.text= notice.name
             holder.textViewDate!!.text= endTime
-            val drawable = holder.imageView!!.resources.getDrawable(R.drawable.ic_baseline_warning_24)
-            if(notice.canDelete!=1) holder.imageView!!.setImageBitmap(drawable.toBitmap())
+            val warnDrawable = holder.imageView!!.resources.getDrawable(R.drawable.ic_baseline_warning_24)
+            val deleteDrawable = holder.imageView!!.resources.getDrawable(R.drawable.ic_baseline_delete_24)
+            if(notice.canDelete!=1) {
+                holder.imageView!!.setImageBitmap(warnDrawable.toBitmap())
+            }
             else{ //可以删除，设置长按监听
+                holder.imageView!!.setImageBitmap(deleteDrawable.toBitmap())
                 holder.itemView.setOnLongClickListener {
-                    AlertDialog.Builder(holder.itemView.context).setTitle("提示").setMessage("您确定删除该事件吗？")
-                        .setPositiveButton("确定", DialogInterface.OnClickListener { _, _ ->
+                    AlertDialog.Builder(holder.itemView.context).setTitle("Warn：").setMessage("是否确定删除事件：${notice.name}")
+                        .setPositiveButton("删除", DialogInterface.OnClickListener { _, _ ->
                             if (notice.itemId != null) {
-                                //todo 发网络请求进行删除
+                                //发网络请求，进行删并更新视图
+                                notice.isFinished?.let { it1 ->
+                                    NotificationsFragment.mContext.deleteNotice(notice.itemId!!,
+                                        it1
+                                    )
+                                }
                             }
-                            //todo 删除完进行更新视图并通知
-                            this.notifyDataSetChanged()
                         })
                         .setNegativeButton("取消", null).show()
                     return@setOnLongClickListener true
@@ -77,8 +88,8 @@ class NotificationsAdapter : RecyclerView.Adapter<NotificationsAdapter.MyViewHol
                 AlertDialog.Builder(holder.itemView.context).setTitle(notice.name).setMessage(notice.detail)
                     .setPositiveButton("返回", DialogInterface.OnClickListener { _, _ ->
                     })
-                    .setNegativeButton("忽略") { _, _ ->
-                        Log.d("TAG", "onBindViewHolder: 忽略")
+                    .setNegativeButton("忽略提醒") { _, _ ->
+                        Log.d("TAG", "onBindViewHolder: 忽略提醒")
                         //TODO 设置不提醒
                     }.show()
             }
--- app/src/main/java/com/example/timetable/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/timetable/ui/notifications/NotificationsFragment.kt
@@ -26,7 +26,9 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class NotificationsFragment : Fragment() {
-
+    companion object{
+        lateinit var mContext :NotificationsFragment
+    }
     private lateinit var notificationsViewModel: NotificationsViewModel
     private var adapter:NotificationsAdapter?= NotificationsAdapter()
     override fun onCreateView(
@@ -41,7 +43,7 @@ class NotificationsFragment : Fragment() {
 //        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
-
+        mContext=this@NotificationsFragment
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,17 +52,16 @@ class NotificationsFragment : Fragment() {
         recyclerView.adapter=adapter
         //先放一个空的adapter，因为网络请求是异步，不然绑定不了adaptor
         adapter?.notifications = mutableListOf()
-        adapter?.let { getNotifications(it,0) }
-
+        getNotifications(0)
         //为按钮设置监听
         val btnTodo = view.findViewById<Button>(R.id.button2)
         val btnDone = view.findViewById<Button>(R.id.button3)
         val addImageView = view.findViewById<ImageView>(R.id.imageViewAdd)
         btnTodo.setOnClickListener {
-            adapter?.let { getNotifications(it,0) }
+            getNotifications(0)
         }
         btnDone.setOnClickListener {
-            adapter?.let { getNotifications(it,1) }
+            getNotifications(1)
         }
         addImageView.setOnClickListener {
             //跳转到activity进行添加
@@ -72,9 +73,9 @@ class NotificationsFragment : Fragment() {
     }
     override fun onStart() {
         super.onStart()
-        adapter?.let { getNotifications(it,0) }
+        getNotifications(0)
     }
-    fun getNotifications(adapter: NotificationsAdapter,isFinished:Int):Unit{
+    fun getNotifications( isFinished:Int):Unit{
         val api = RetrofitUtils.getRetrofit().create(ItemApi::class.java)
         api.selectItems(GlobalMsg.info.userId!!,isFinished)
             .enqueue(object : Callback<ItemBean> {
@@ -86,8 +87,8 @@ class NotificationsFragment : Fragment() {
                                     Toast.makeText(context, "当前无任何通知事件", Toast.LENGTH_LONG).show()
                                 else {
                                     Log.d("TAG", "onResponse:${it.data} ")
-                                    adapter.notifications = it.data!!
-                                    adapter.notifyDataSetChanged()
+                                    adapter?.notifications = it.data!!
+                                    adapter?.notifyDataSetChanged()
                                 }
                             } else {
                                 Toast.makeText(context, "网络请求失败，请稍后再试", Toast.LENGTH_LONG).show()
@@ -96,6 +97,28 @@ class NotificationsFragment : Fragment() {
                     }
                 }
 
+                override fun onFailure(call: Call<ItemBean>, t: Throwable) {
+                    Toast.makeText(context, "网络请求失败，请稍后再试！", Toast.LENGTH_LONG).show()
+
+                }
+            })
+    }
+    fun deleteNotice(itemId:Int,isFinished: Int):Unit{
+        val api = RetrofitUtils.getRetrofit().create(ItemApi::class.java)
+        api.deleteItem(itemId)
+            .enqueue(object : Callback<ItemBean> {
+                override fun onResponse(call: Call<ItemBean>, response: Response<ItemBean>) {
+                    response.let { it ->
+                        it.body()?.let {
+                            if (it.code == 200) {
+                                Toast.makeText(context, "删除成功", Toast.LENGTH_LONG).show()
+                                getNotifications(isFinished) //删除成功后刷新视图
+                            } else {
+                                Toast.makeText(context, "网络请求失败，请稍后再试", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+                }
                 override fun onFailure(call: Call<ItemBean>, t: Throwable) {
                     Toast.makeText(context, "网络请求失败，请稍后再试！", Toast.LENGTH_LONG).show()
 
